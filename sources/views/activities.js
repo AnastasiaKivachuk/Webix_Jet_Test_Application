0e88d181e68dfb0ffedb8b1c97c26b11bb23054c
anastasia_kivachuk <anastasia.kivachuk@gmail.com>
2019-07-04 11:09:31 +0300

    added language localisation

--- sources/views/activities.js
+++ sources/views/activities.js
@@ -51,7 +51,12 @@ export default class ActivityView extends JetView {
 								id: 7,
 								value: "This month"
 							}
-						]
+						],
+						on: {
+							onChange: () => {
+								this.$$("contactsData").filterByAll();
+							}
+						}
 					},
 					{
 
@@ -163,5 +168,46 @@ export default class ActivityView extends JetView {
 				Activity.data.filter();
 			}
 		);
+
+		this.$$("contactsData").registerFilter(
+			webix.$$("tabbar"),
+			{
+				columnId: "State",
+				compare: (value, filter) => {
+					let filterData = parseInt(filter);
+					if (filterData === 1) return value;
+					else if (filterData === 2) return value === "Open";
+					else if (filterData === 3) return value === "Close";
+					else if (filterData === 4) console.log(4);
+					else if (filterData === 5) console.log(5);
+					else if (filterData === 6) console.log(6);
+					return value === "Close";
+				}
+
+
+				// columnId: "DueNewDate",
+				// compare: (value, filter) => {
+				// 	let year = value.getFullYear().toString();
+				// 	let filterData = parseInt(filter);
+				// 	console.log(value);
+				// 	console.log(filterData);
+				// 	if (filterData === 1) return year;
+				// 	// else if (filterData === 2) return value === "Open";
+				// 	// else if (filterData === 3) return value === "Close";
+				// 	else if (filterData === 4) return year === new Date().getFullYear().toString();
+				// 	else if (filterData === 5) console.log(5);
+				// 	else if (filterData === 6) console.log(7);
+				// 	return value === "Close";
+				// }
+			},
+			{
+				getValue: node => node.getValue(),
+				setValue: (node, value) => {
+					node.setValue(value);
+				}
+			}
+		);
 	}
+
+
 }
--- sources/views/settings.js
+++ sources/views/settings.js
@@ -4,10 +4,33 @@ import {
 
 export default class SettingsView extends JetView {
 	config() {
+		const _ = this.app.getService("locale")._;
+		const lang = this.app.getService("locale").getLang();
+
 		return {
-			view: "template",
-			template: "Settings"
+			rows: [
+				{
+					name: "lang",
+					optionWidth: 120,
+					view: "segmented",
+					label: _("Language"),
+					options: [
+						{id: "en", value: "English"},
+						{id: "ru", value: "Russian"}
+					],
+					click: () => this.toggleLanguage(),
+					value: lang},
+				{}
+			]
+
 		};
+	}
+
+	toggleLanguage() {
+		const langs = this.app.getService("locale");
+		const value = this.getRoot().queryView({name: "lang"}).getValue();
+		langs.setLang(value);
+		console.log(value);
 	}
 }
 
