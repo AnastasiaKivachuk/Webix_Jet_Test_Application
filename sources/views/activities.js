3fca5bc73f988fb94287f5d2b377fb4a486c4406
anastasia_kivachuk <anastasia.kivachuk@gmail.com>
2019-07-07 10:16:55 +0300

    update filter

--- sources/views/activities.js
+++ sources/views/activities.js
@@ -17,141 +17,139 @@ export default class ActivityView extends JetView {
 		const _ = this.app.getService("locale")._;
 		return {
 			rows: [{
-				cols: [
-					{
-						view: "tabbar",
-						id: "tabbar",
-						autodidth: true,
-						options: [
-							{
-								id: 1,
-								value: _("All")
-							},
-							{
-								id: 2,
-								value: _("Overdue")
-							},
-							{
-								id: 3,
-								value: _("Completed")
-							},
-							{
-								id: 4,
-								value: _("Today")
-							},
-							{
-								id: 5,
-								value: _("Tomorrow")
-							},
-							{
-								id: 6,
-								value: _("This week")
-							},
-							{
-								id: 7,
-								value: _("This month")
-							}
-						],
-						on: {
-							onChange: () => {
-								this.$$("contactsData").filterByAll();
-							}
-						}
-					},
-					{
-
-						view: "button",
-						type: "icon",
-						css: "webix_primary",
-						icon: "wxi-plus",
-						label: _("Add activity"),
-						autowidth: true,
-						click: () => {
-							this.form.showForm({}, _("Add"));
-						}
-
-					}
-				]
-			},
-			{
-				view: "datatable",
-				localId: "contactsData",
-				hover: "hoverLine",
-				autoConfig: true,
-				scrollX: false,
-				select: true,
-				columns: [{
-					id: "State",
-					header: "",
-					checkValue: "Close",
-					uncheckValue: "Open",
-					template: "{common.checkbox()}",
-					editor: "checkbox"
-
-				},
-				{
-					id: "TypeID",
-					editor: "select",
-					header: ["Activity type", {
-						content: "selectFilter"
-					}],
-					options: ActivityType,
-					width: 300,
-					sort: "string"
-				},
-				{
-					id: "DueNewDate",
-					format: webix.i18n.longDateFormatStr,
-					header: [_("Due date"), {
-						content: "dateRangeFilter"
-					}],
-					width: 300,
-					sort: "date"
-				},
-				{
-					id: "Details",
-					header: [_("Details"), {
-						content: "textFilter"
-					}],
-					fillspace: true,
-					sort: "string"
-				},
-				{
-					id: "ContactID",
-					editor: "select",
-					header: [_("Contact"), {
-						content: "selectFilter"
-					}],
-					options: Contacts,
-					width: 300,
-					sort: "string"
-				},
-				{
-					template: "<span class='webix_icon wxi-pencil editBtn'></span>",
-					width: 40
-				},
-				{
-					template: "<span class='webix_icon wxi-trash removeBtn'></span>",
-					width: 40
-				}
-				],
-				onClick: {
-					removeBtn: (el, id) => {
-						webix.confirm({
-							text: _("Do you still want to continue?"),
-							callback: (result) => {
-								if (result) {
-									Activity.remove(id);
+					cols: [{
+							view: "tabbar",
+							id: "tabbar",
+							autodidth: true,
+							options: [{
+									id: 1,
+									value: _("All")
+								},
+								{
+									id: 2,
+									value: _("Overdue")
+								},
+								{
+									id: 3,
+									value: _("Completed")
+								},
+								{
+									id: 4,
+									value: _("Today")
+								},
+								{
+									id: 5,
+									value: _("Tomorrow")
+								},
+								{
+									id: 6,
+									value: _("This week")
+								},
+								{
+									id: 7,
+									value: _("This month")
+								}
+							],
+							on: {
+								onChange: () => {
+									this.$$("contactsData").filterByAll();
 								}
 							}
-						});
-						return false;
-					},
-					editBtn: (el, id) => {
-						this.form.showForm(Activity.getItem(id), _("Edit"));
+						},
+						{
+
+							view: "button",
+							type: "icon",
+							css: "webix_primary",
+							icon: "wxi-plus",
+							label: _("Add activity"),
+							autowidth: true,
+							click: () => {
+								this.form.showForm({}, _("Add"));
+							}
+
+						}
+					]
+				},
+				{
+					view: "datatable",
+					localId: "contactsData",
+					hover: "hoverLine",
+					autoConfig: true,
+					scrollX: false,
+					select: true,
+					columns: [{
+							id: "State",
+							header: "",
+							checkValue: "Close",
+							uncheckValue: "Open",
+							template: "{common.checkbox()}",
+							editor: "checkbox"
+
+						},
+						{
+							id: "TypeID",
+							editor: "select",
+							header: ["Activity type", {
+								content: "selectFilter"
+							}],
+							options: ActivityType,
+							width: 300,
+							sort: "string"
+						},
+						{
+							id: "DueNewDate",
+							format: webix.i18n.longDateFormatStr,
+							header: [_("Due date"), {
+								content: "dateRangeFilter"
+							}],
+							width: 300,
+							sort: "date"
+						},
+						{
+							id: "Details",
+							header: [_("Details"), {
+								content: "textFilter"
+							}],
+							fillspace: true,
+							sort: "string"
+						},
+						{
+							id: "ContactID",
+							editor: "select",
+							header: [_("Contact"), {
+								content: "selectFilter"
+							}],
+							options: Contacts,
+							width: 300,
+							sort: "string"
+						},
+						{
+							template: "<span class='webix_icon wxi-pencil editBtn'></span>",
+							width: 40
+						},
+						{
+							template: "<span class='webix_icon wxi-trash removeBtn'></span>",
+							width: 40
+						}
+					],
+					onClick: {
+						removeBtn: (el, id) => {
+							webix.confirm({
+								text: _("Do you still want to continue?"),
+								callback: (result) => {
+									if (result) {
+										Activity.remove(id);
+									}
+								}
+							});
+							return false;
+						},
+						editBtn: (el, id) => {
+							this.form.showForm(Activity.getItem(id), _("Edit"));
+						}
 					}
 				}
-			}
 			]
 		};
 	}
@@ -170,8 +168,7 @@ export default class ActivityView extends JetView {
 		);
 
 		this.$$("contactsData").registerFilter(
-			webix.$$("tabbar"),
-			{
+			webix.$$("tabbar"), {
 				columnId: "State",
 				compare: (value, filter, item) => {
 					let filterData = parseInt(filter);
@@ -187,18 +184,35 @@ export default class ActivityView extends JetView {
 					let tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
 
 					let currentDayOfWeek = new Date().getDay();
-					let firstDayOfWeek = new Date(new Date().getTime() - 24 * 60 * 60 * 1000 * currentDayOfWeek);
-					let lastDayOfWeek = new Date(new Date().getTime() + 24 * 60 * 60 * 1000 * (6 - currentDayOfWeek));
+					// console.log(currentDayOfWeek);
+
+					let firstDayOfWeek = new Date(new Date().getTime() - 24 * 60 * 60 *
+						1000 * currentDayOfWeek);
+					firstDayOfWeek.setHours(0, 0, 0, 0);
+					let lastDayOfWeek = new Date(new Date().getTime() + 24 * 60 * 60 *
+						1000 * (6 - currentDayOfWeek));
+					lastDayOfWeek.setHours(0, 0, 0, 0);
+					// console.log(firstDayOfWeek);
+					// console.log(lastDayOfWeek);
 					if (filterData === 1) return item;
-					else if (filterData === 2) return state === "Open" && date < new Date();
-					else if (filterData === 3) return state === "Close";
-					else if (filterData === 4) return yearDate === currentYear && monthDate === currentMonth && dayDate === currentDay;
-					else if (filterData === 5) return yearDate === tomorrow.getFullYear() && monthDate === tomorrow.getMonth() && dayDate === tomorrow.getDate();
-					else if (filterData === 6) return date >= firstDayOfWeek && date <= lastDayOfWeek;
+					else if (filterData === 2) {
+						return state === "Open" && date <
+							new Date();
+					} else if (filterData === 3) return state === "Close";
+					else if (filterData === 4) {
+						return yearDate === currentYear &&
+							monthDate === currentMonth && dayDate === currentDay;
+					} else if (filterData === 5) {
+						return yearDate === tomorrow
+							.getFullYear() && monthDate === tomorrow.getMonth() &&
+							dayDate === tomorrow.getDate();
+					} else if (filterData === 6) {
+						return date >= firstDayOfWeek && date <=
+							lastDayOfWeek;
+					}
 					return yearDate === currentYear && monthDate === currentMonth;
 				}
-			},
-			{
+			}, {
 				getValue: node => node.getValue(),
 				setValue: (node, value) => {
 					node.setValue(value);
@@ -206,6 +220,4 @@ export default class ActivityView extends JetView {
 			}
 		);
 	}
-
-
 }
